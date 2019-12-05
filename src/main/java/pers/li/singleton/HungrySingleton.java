ebb9291473c46ef9e195291bafffb82cbedab3e2
a982338665 <982338665@qq.com>
2019-12-05 16:53:23 +0800

    线程安全的单例模式

---
+++ src/main/java/pers/li/singleton/HungrySingleton.java
@@ -0,0 +1,21 @@
+package pers.li.singleton;
+
+/**
+ * 饿汉式：类加载时初始化，线程安全
+ *      优点：没有加锁，执行效率会提高。
+ *      缺点：类加载时就初始化，分配内存。
+ *      基于 classloader 机制避免了多线程的同步问题，
+ */
+public class HungrySingleton {
+
+    //1.构造方法私有化
+    private HungrySingleton(){}
+
+    //2.私有静态属性
+    private static HungrySingleton hungrySingleton = new HungrySingleton();
+
+    //3.对外公开获取方法
+    public static HungrySingleton getInstance(){
+        return hungrySingleton;
+    }
+}
